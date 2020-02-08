99600a081f348a7d35799e29b79519e8be6d5f45
Ian Odundo <38793943+poppinjay13@users.noreply.github.com>
2020-02-08 15:14:27 +0300

    create event ui

--- app/src/main/java/com/poppinjay13/grind/CreateActivity.java
+++ app/src/main/java/com/poppinjay13/grind/CreateActivity.java
@@ -1,14 +1,79 @@
 package com.poppinjay13.grind;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateActivity extends AppCompatActivity {
 
+    LinearLayout dateLayout, timeLayout;
+    ImageView back, set_date, set_time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        dateLayout = findViewById(R.id.date_data);
+        timeLayout = findViewById(R.id.time_data);
+        set_date = findViewById(R.id.set_date);
+        set_date.setTag("no_show");
+        set_time = findViewById(R.id.set_time);
+        set_time.setTag("no_show");
+        setListeners();
+    }
+
+    private void setListeners() {
+        set_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String image = (String) set_date.getTag();
+                if (image.equals("no_show")) {
+                    set_date.setImageResource(R.drawable.calendar_selected);
+                    dateLayout.setVisibility(View.VISIBLE);
+                    set_date.setTag("show");
+                } else{
+                    set_date.setImageResource(R.drawable.calendar);
+                    dateLayout.setVisibility(View.GONE);
+                    set_date.setTag("no_show");
+                }
+            }
+        });
+
+        set_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String image = (String) set_time.getTag();
+                if (image.equals("no_show")) {
+                    set_time.setImageResource(R.drawable.clock_selected);
+                    timeLayout.setVisibility(View.VISIBLE);
+                    set_time.setTag("show");
+                } else{
+                    set_time.setImageResource(R.drawable.clock);
+                    timeLayout.setVisibility(View.GONE);
+                    set_time.setTag("no_show");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onNavigateUp() {
+        finish();
+        return true;
     }
 }
